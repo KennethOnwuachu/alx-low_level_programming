62d13149a5960675f2281c38ad0705f30ad6661a
Kenneth Onwuachu <Kennethonwuachu@gmail.com>
2022-06-30 08:42:39 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main -Entry point
+ *
+ * Return: always 1 (Success)
+ */
+int main(void)
+{
+	char strl[] = "and that piece of art is useful\" -Dora Korpar,2015-10-19\n";
+
+	write(2, strl, 59);
+	return (1);
+}

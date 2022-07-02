effe1cd13b3c13b5076745aa95255024d988e30c
Kenneth Onwuachu <Kennethonwuachu@gmail.com>
2022-07-02 14:38:34 -0700

    update

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,9 +3,9 @@
 /**
  * main - Entry point
  *
- * Description -  prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ * Description -  prints all single digit numbers 0 to 10 
  *
- * Return ; Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)

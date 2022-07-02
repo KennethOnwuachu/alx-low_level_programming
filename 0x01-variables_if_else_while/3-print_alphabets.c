4fa25952e60a4a2425d2d2d48fa57caff6524d29
Kenneth Onwuachu <Kennethonwuachu@gmail.com>
2022-07-02 13:41:16 -0700

    update2

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,7 +14,7 @@ int main(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-	for (letter = 'a'; letter <= 'Z'; letter++)
+	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 	putchar('\n');
 	return (0);

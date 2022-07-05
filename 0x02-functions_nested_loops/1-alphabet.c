2f235a0cf8df27f36f88ff1641ca48b9ea9074d9
Kenneth Onwuachu <Kennethonwuachu@gmail.com>
2022-07-05 14:55:07 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,12 @@
 /*
  * main -Entry point
  *
- * Description : This function prints alphabetsin lower cases
+ * print_alphabet : This function prints alphabetsin lower cases
  *
  * Return : Always 0 (Success)
  */
 
 void print_alphabet(void)
-/*
- * prints alphabet
- */
 {
 	char c;
 

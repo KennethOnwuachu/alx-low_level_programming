d69241a345e344ac5b1db04e1d7a74d8e6efd23f
Kenneth Onwuachu <Kennethonwuachu@gmail.com>
2022-07-05 07:41:03 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Description: A program that prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 2;
+
+	if (i > 1)
+		printf("_putchar\n");
+
+	return (0);
+}
+

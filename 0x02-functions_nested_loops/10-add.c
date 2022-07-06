9dac570805c3bd9fcbb295833e6830d518ad7b0d
Kenneth Onwuachu <Kennethonwuachu@gmail.com>
2022-07-06 04:37:46 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - addition
+ * @a: integer 1 to be added
+ * @b: integer 2 to be added
+ *
+ * return: check code
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}

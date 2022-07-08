b3372e43f3a78a9fc7c43dd50ef5ae55467c564e
KennethOnwuachu <kennethonwuachu@gmail.com>
2022-07-08 22:12:44 +0000

    isdigit betty ish

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* is_digit - checks for a digit 0 - 9
-* @c : input character
+*_isdigit - checks for a digit 0 - 9
+* @c : input number
 * Return: 1 if c is a digit, 0 otherwise
 */
 int _isdigit(int c)

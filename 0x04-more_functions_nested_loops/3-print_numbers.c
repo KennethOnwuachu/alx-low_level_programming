2ed9750b791835692ac47440693fd3a2b619ce36
KennethOnwuachu <kennethonwuachu@gmail.com>
2022-07-08 23:11:57 +0000

    okkk

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,11 @@
 void print_numbers(void)
 {
 	int ch;
+	ch = 0;
 
-	for (ch = '0'; ch < '10'; ch++)
+	for (ch = 0; ch < 10; ch++)
 	{
-		_putchar(ch);
+		_putchar(ch + '0');
 	}
 	_putchar('\n');
 }

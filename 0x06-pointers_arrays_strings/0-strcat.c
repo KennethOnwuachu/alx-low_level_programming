3e99cafe56a2a1a492ec9f9c406caf679e8a134c
KennethOnwuachu <kennethonwuachu@gmail.com>
2022-07-18 12:18:36 +0000

    betty wahala again

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,8 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
-	for (j = 0; dest[j] != '\0'; j++);
+	for (j = 0; dest[j] != '\0'; j++)
+		;
 	for (i = 0; dest[i] != '\0'; i++, j++)
 	{
 		dest[j] = src[i];
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+* _strcmp: this is a function that compares strings
+* @s1- string 1
+* @s2- sting 2
+*
+*Return : nothing 

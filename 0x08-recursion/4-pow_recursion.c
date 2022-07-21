8d9524b5460f7baf6543e2379ed2392d0ce1faf2
KennethOnwuachu <kennethonwuachu@gmail.com>
2022-07-21 23:04:05 +0000

    haba

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,17 +3,17 @@
 /**
 * _pow_recursion - checks the power of one number to another
 * @x : base
-* @y : exponent 
+* @y : exponent
 * Return:  the value of x raised to the power of y
 */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else if (y == 0)
-		return (1);
-	else
+	else if
 	{
-		return ( x * _pow_recursion(x , y - 1));
+		return (x * _pow_recursion(x , y - 1));
 	}
+	else
+		return (0);
 }

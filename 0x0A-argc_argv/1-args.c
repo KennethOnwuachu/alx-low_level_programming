c11a90b64b4e40ac62ea795b648a91b851457445
KennethOnwuachu <kennethonwuachu@gmail.com>
2022-07-23 16:47:57 +0000

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main- program that prints the number of arguments
+* @argc: counts the number
+* @argv: the arguments that are passed
+*
+* Return: Always (0) success
+*/
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	int c;
+	c = argc;
+	printf("%d\n", c - 1);
+	return (0);
+}

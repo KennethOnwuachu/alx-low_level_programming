f6eeba25b4c90399da26eace22eaae3432d5c576
KennethOnwuachu <kennethonwuachu@gmail.com>
2022-07-23 10:42:27 +0000

    power recursion fixed

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,8 +8,13 @@
 */
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
-		return (1 + _strlen_recursion(s + 1));
-	else
+	int n;
+
+	if (s[0] == '\0')
 		return (0);
+	else
+	{
+		n = (1 +  _strlen_recursion(s + 1));
+		return (n);
+	}
 }

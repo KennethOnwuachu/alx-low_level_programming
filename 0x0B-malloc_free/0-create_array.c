c66cc46db41ab18e050d221226ad39b92c1a7065
KennethOnwuachu <kennethonwuachu@gmail.com>
2022-08-03 13:57:01 +0000

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Main - function that creates an array of chars and initializes it
+* @size : unsigned int
+* @c : char
+* Return: null if size is 0
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int s;
+	char *cr;
+
+	if (size == 0)
+		return (NULL);
+
+	cr = malloc(size * sizeof(c));
+
+	if (cr == NULL)
+		return (NULL);
+
+	for (s = 0; s < size; s++)
+		cr[s] = c;
+
+	return (cr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This is all about malloc and free. This is supposed to help you learn how to use dynamic memory allocation

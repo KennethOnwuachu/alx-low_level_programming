de91ac59fb6ec045fea5292cf304fc1ba6058287
KennethOnwuachu <kennethonwuachu@gmail.com>
2022-08-27 09:14:29 +0000

    fixed

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,13 +7,15 @@
 * Return: returns a character
 */
 
-char *cap_string(char *)
+char *cap_string(char *str)
 {
 	int i;
 	int j;
 	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 
-	for (i = 0; str[i] != '\0'; i++)
+	i = 0;
+
+	while (str[i] != '\0')
 	{
 		if (str[i] >= 97 && str[i] <= 122)
 		{
